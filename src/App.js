61b4d84db36e721eac2396f14d9ccd104aee8477
Danish <danishriz100@gmail.comgit commitexit>
2021-04-12 11:27:42 -0600

    fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(
+        "https://orangevalleycaa.org/api/videos"
+      ).then((response) => response.json());
+
+      setData(result);
+    };
+
+    fetchData();
+  },[]); // using square bracket to fetch data only when component mounts
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Videos</h1>
       </header>
+
+      {
+        data.map(video => (
+
+          <div>
+            <h2>
+              {video.name}
+            </h2>
+          </div>
+        ))
+      }
     </div>
   );
 }
